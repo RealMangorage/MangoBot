5caa5c100922a18556264a1b3a80b68bcd73e9bd
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2023-07-18 03:03:50 -0700

    Licencing added.

--- src/main/java/org/mangorage/mangobot/commands/ReplyEmbedCommand.java
+++ src/main/java/org/mangorage/mangobot/commands/ReplyEmbedCommand.java
@@ -1,7 +1,30 @@
+/*
+ * Copyright (c) 2023. MangoRage
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package org.mangorage.mangobot.commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
+
 import java.awt.*;
 import java.sql.Time;
 import java.time.Instant;
@@ -11,18 +34,18 @@ public class ReplyEmbedCommand extends AbstractEmbedCommand {
     public CommandResult execute(Message message, String... args) {
         message.replyEmbeds(
                 new EmbedBuilder()
-                .setTitle("Title")
-                .setAuthor("MangoRage")
-                .setImage("https://preview.redd.it/rcpgibdaqkl61.png?width=1920&format=png&auto=webp&s=c5b0c957843c37c6a7b6cc734c3934465c955415")
-                .setFooter("Footer")
-                .setColor(Color.WHITE)
-                .setDescription("Description")
-                .setThumbnail("https://oyster.ignimgs.com/mediawiki/apis.ign.com/minecraft/b/b4/Herobrine.png")
-                .appendDescription("Additional Description")
-                .addField("test", "value", false)
-                .addField("test", "value inline", true)
-                .setTimestamp(Time.from(Instant.now()).toInstant())
-                .build()).queue();
+                        .setTitle("Title")
+                        .setAuthor("MangoRage")
+                        .setImage("https://preview.redd.it/rcpgibdaqkl61.png?width=1920&format=png&auto=webp&s=c5b0c957843c37c6a7b6cc734c3934465c955415")
+                        .setFooter("Footer")
+                        .setColor(Color.WHITE)
+                        .setDescription("Description")
+                        .setThumbnail("https://oyster.ignimgs.com/mediawiki/apis.ign.com/minecraft/b/b4/Herobrine.png")
+                        .appendDescription("Additional Description")
+                        .addField("test", "value", false)
+                        .addField("test", "value inline", true)
+                        .setTimestamp(Time.from(Instant.now()).toInstant())
+                        .build()).queue();
         return CommandResult.PASS;
     }
 }
